b3dbcdd645d4330e68feae78e8d04b68d1df9f98
sanucp <anavarro@ebsco.com>
2021-05-20 14:59:44 -0500

    update the port - 2

--- src/server/index.js
+++ src/server/index.js
@@ -592,6 +592,6 @@ app.get("/api/request/:i&:a&:us&:bib&:cid", (req, res) => {
   }
 });
 
-app.listen(process.env.PORT || 8080, () =>
+app.listen(process.env.PORT || 8081, () =>
   console.log(`Listening on port ${process.env.PORT || 8081}!`)
 );
